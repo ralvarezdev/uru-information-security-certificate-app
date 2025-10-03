e0ba1f582b1414a8261cc01de95501b6eec07f3b
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2025-10-02 21:24:32 -0400

    feat: added client setup for certificate microservice

---
+++ microservice/grpc/certificate.py
@@ -0,0 +1,18 @@
+import grpc
+
+from ralvarezdev import certificate_pb2_grpc
+
+def create_grpc_client(host: str, port: int):
+    """
+    Creates and returns a gRPC client stub.
+
+	Args:
+		host (str): The server host.
+		port (int): The server port.
+
+	Returns:
+		certificate_pb2_grpc.CertificateStub: The gRPC client stub.
+    """
+    channel = grpc.insecure_channel(f"{host}:{port}")
+    stub = certificate_pb2_grpc.CertificateStub(channel)
+    return stub
